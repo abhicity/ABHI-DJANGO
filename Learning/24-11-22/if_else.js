a04f1fbe3ec7612355e979a8144d2bd6f9d1927a
abhicity <machcityabhi@gmail.com>
2022-11-24 20:54:15 +0530

    Learning

---
+++ Learning/24-11-22/if_else.js
@@ -0,0 +1,11 @@
+var temperature = 32;
+
+if (temperature < 20) {
+    console.log("Temperature is COLD");
+}
+else if (temperature == 20) {
+    console.log("Temperature is PLEASENT");
+}
+else {
+    console.log("Temperature is HOT");
+}
